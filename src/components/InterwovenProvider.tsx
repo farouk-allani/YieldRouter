60a78e6b5c4bcc16e9bb294ee16378765eac5917
farouk-allani <farouk-allani@users.noreply.github.com>
2026-04-12 22:25:24 +0100

    fix: remove unnecessary InterwovenKit component from InterwovenProvider

--- src/components/InterwovenProvider.tsx
+++ src/components/InterwovenProvider.tsx
@@ -47,7 +47,6 @@ export default function InterwovenProvider({ children }: { children: ReactNode }
           enableAutoSign={true}
         >
           {children}
-          <InterwovenKit />
         </InterwovenKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
